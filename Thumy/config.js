2b8d14e497da868db968c0b6d43669adcf07b97c
qu4nhuynh-son <116681639+qu4nhuynh-son@users.noreply.github.com>
2022-12-03 18:41:34 -0500

    Add files via upload

---
+++ Thumy/config.js
@@ -0,0 +1,20 @@
+// 1. Chỉnh sửa các dòng chữ theo mục đích của bạn
+const CONFIG = {
+    introTitle: '🖐🖐🖐 Hé lô ông già của anh! 🖐🖐🖐',
+    introDesc: `🎄🎄🎄🎄Chúc em giáng sinh vui vẻ, luôn vui vẻ và hạnh phúc 🎄🎄🎄🎄, 
+    thật ra thì anh có nhiều thứ muốn nói với em lắm
+    anh muốn nói trực tiếp bằng lời nói hơn, và anh muốn nhìn ngắm khuôn mặt của em để nói lời này, không biết em cho anh cơ hội được không ?, anh biết lỗi của mình lắm rồi, anh đã suy nghĩ rất nhiều về chuyện 2 đứa mình nhiều lắm và anh muốn hỏi là:    `,
+    btnIntro: 'OK',
+    title: 'Vậy em cho anh hỏi là, hỏi là em nè, em cho anh cơ hội làm em được hạnh phúc 1 lần nữa được không ? 😙',
+    desc: 'Em không trả lời hoặc thoát ra, tức là Em chịu đó nha, ông già :> ',
+    btnYes: 'Thôi được rồi, tui cho ông cơ hội đó nhưng mà phải thương tui gấp 1000000 lần nhe 💖💖💖',
+    btnNo: '💔 Anh mơ à ! 💔 ',
+    question:'Thế em đồng ý rồi hả, thiệt không đó trời anh mừng quá 😊😊',
+    reasonPlaceholder: 'Lý do',
+    btnReply: 'Gởi ! 💌💌',
+    reply: 'Tại vì anh đáng yêu và chân thành 😊😊',
+    mess: 'Anh biết mà, Anh sẽ cố gắng nỗ lực yêu hơn hơn gấp 100 lần số trên 🥰. Anh yêu em vợ  😘😘',
+    messDesc: '💬💬 Em gọi cho anh được không cục vàng ơi. 💬💬',
+    btnAccept: 'Ok lun 💖',
+    messLink: 'https://m.me/huynhphucminhquan'
+}
